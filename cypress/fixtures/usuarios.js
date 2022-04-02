bd14d4263a67b48b6a012a88ad07d0f4533e582b
castellanlucas <castellanlucas@gmail.com>
2022-04-01 23:47:33 -0300

    Desafio take

---
+++ cypress/fixtures/usuarios.js
@@ -0,0 +1,8 @@
+export const ELEMENTS = {
+
+    "userPadraoUat": '97540896', 
+    "senhaPadraoUat": "J@va2501",
+    "userV8": 'input#loginPassword',
+    "senhaUserV8": 'button#loginFormSubmit'
+
+} 
---
+++ support/pages/Menu_produtos/elements_menuProd.js
@@ -0,0 +1,12 @@
+export const ELEMENTS = { 
+    
+    // Sem tour 
+    "banner": '#banner_close',
+
+
+    // Com tour 
+
+    "btnIniciarTour": '[launchsection="modalWalkthrough"]', 
+    "tourBoletos_Faturas": '#step-wizard-1'
+
+}
